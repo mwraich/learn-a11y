fb81f274eae6c7211a55dfdad8f7a32db61c7e25
Meep <mwraich28@gmail.com>
2021-03-12 18:38:08 -0500

    Restrict tabbing to just inside of the modal

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
-var modal        = document.querySelector('.focus-modal');
-var modalButton  = document.querySelector('.focus-modal-button');
+var modal = document.querySelector('.focus-modal');
+var modalButton = document.querySelector('.focus-modal-button');
 var modalOverlay = document.querySelector('.focus-modal-overlay');
 var cancelButton = document.querySelector('.focus-modal-cancel');
 
@@ -10,13 +10,47 @@ cancelButton.addEventListener('click', close);
 // https://github.com/jkup/focusable
 
 function open() {
-  // Show the modal and overlay
-  modal.style.display = 'block';
-  modalOverlay.style.display = 'block';
+	var previouslyFocused = document.activeElement;
+	var focusableElements = modal.querySelectorAll(
+		'a[href], area[href], input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary, [tabindex^="0"], [tabindex^="1"], [tabindex^="2"], [tabindex^="3"], [tabindex^="4"], [tabindex^="5"], [tabindex^="6"], [tabindex^="7"], [tabindex^="8"], [tabindex^="9"]'
+	);
+	// turn into an array
+	focusableElements = Array.prototype.slice.call(focusableElements);
+
+	var firstItem = focusableElements[0];
+	var lastItem = focusableElements[focusableElements.length - 1];
+
+	modal.addEventListener('keydown', trap);
+
+	// Show the modal and overlay
+	modal.style.display = 'block';
+	modalOverlay.style.display = 'block';
+
+	function trap(event) {
+		if (event.keyCode === 9) {
+			// shift is held down
+			if (event.shiftKey) {
+				// Backwards
+				if (document.activeElement === firstItem) {
+					event.preventDefault();
+					lastItem.focus();
+				}
+			} else {
+				if (document.activeElement === lastItem) {
+					event.preventDefault();
+					firstItem.focus();
+				}
+			}
+		} else if (event.keyCode === 27) {
+			// escape
+			console.log('test');
+			close();
+		}
+	}
 }
 
 function close() {
-  // Hide the modal and overlay
-  modal.style.display = 'none';
-  modalOverlay.style.display = 'none';
+	// Hide the modal and overlay
+	modal.style.display = 'none';
+	modalOverlay.style.display = 'none';
 }
